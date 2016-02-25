787d45711923ac29a4eef44095b847907f109d1b
swfree <skye.free@gmail.com>
2016-02-25 13:31:22 -0800

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/cities', function(request, response) {
+  var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+  response.json(cities);
+});
+
+module.exports = app;
